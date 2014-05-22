ee898f323c5152ee98532253a48da1e9d9f92ca0
Jack Mallers <jimmymowschess@gmail.com>
2014-05-22 18:11:33 -0500

    trying to set up cached pieces

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 ChessExplained::Application.configure do
   config.middleware.delete Rack::Lock
   # Settings specified here will take precedence over those in config/application.rb.
+  config.static_cache_control = "public, max-age=2592000"
+
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development

ac9c5a1a5059660fa3eaa44c09111c515dc30e53
Jack Mallers <jimmymowschess@gmail.com>
2014-06-09 11:53:48 -0500

    pushing

--- config/application.rb
+++ config/application.rb
@@ -6,17 +6,19 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-  ENV[key] = value unless value.kind_of? Hash
-end
+# config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+# config.merge! config.fetch(Rails.env, {})
+# config.each do |key, value|
+#   ENV[key] = value unless value.kind_of? Hash
+# end
 
 module ChessExplained
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/lib/* #{config.root}/lib/*/* )
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.

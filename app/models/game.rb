c943bb9a958e72ffe1121e7b04331efc9ff3975f
Jack Mallers <jackmallers@Jacks-MacBook-Pro.local>
2014-06-29 23:02:08 -0500

    fixed little variations bug

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,16 +7,14 @@ class Game < ActiveRecord::Base
 
   def create_moves_with_parsed_notation(moves_object)
     moves_object.each do |move|
-      db_move = Move.create(notation: move['notation'], fen: move['fen'], game_id: self.id)
-
       if move['variation']
-        variation = Variation.create(move_id: db_move.id)
+        variation = Variation.create(move_id: self.moves.last.id)
 
         move['variation'].each do |variation_move|
           VariationMove.create(variation_id: variation.id, notation: variation_move['notation'], fen: variation_move['fen'])
         end
-
       end
+      db_move = Move.create(notation: move['notation'], fen: move['fen'], game_id: self.id)
     end
   end
 end
